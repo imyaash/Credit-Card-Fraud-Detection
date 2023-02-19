60052abdac448fdf011323d63c6da8bbb016dbae
Yash Panchal <63871652+imyaash@users.noreply.github.com>
2023-02-19 15:22:49 +0000

    Adding requirement.bat

---
+++ requirement.bat
@@ -0,0 +1,30 @@
+@echo off
+
+echo Installing numpy...
+pip install numpy
+
+echo Installing pandas...
+pip install pandas
+
+echo Installing seaborn...
+pip install seaborn
+
+echo Installing matplotlib...
+pip install matplotlib
+
+echo Installing sklearn...
+pip install scikit-learn
+
+echo Installing imblearn...
+pip install imbalanced-learn
+
+echo Installing pickle...
+pip install pickle
+
+echo Installing tkinter...
+pip install tkinter
+
+echo Installing argparse...
+pip install argparse
+
+echo All libraries installed successfully.
